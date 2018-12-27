370df3e1a7b0c0d5c03656510318e743c944d41c
klee128 <46151239+klee128@users.noreply.github.com>
2018-12-26 18:45:30 -0800

    Add files via upload

---
+++ Word.java
@@ -0,0 +1,245 @@
+import java.util.Scanner;
+
+public class Word {
+	
+	public static void main(String[] args) {
+	
+	
+	Scanner sc = new Scanner(System.in);
+	int wat = sc.nextInt();
+	int counter = wat;
+	
+	for (int why = 0; why < wat; why++) {
+		
+		String word = sc.next();
+		
+		int a = 0;
+		for( int some=0; some<word.length(); some++ ) {
+		    if( word.charAt(some) == 'a' ) {
+		        a++;
+		    } 
+		}
+		if(a>1) {
+			counter--;
+		}
+	
+		int b = 0;
+		for( int some=0; some<word.length(); some++ ) {
+		    if( word.charAt(some) == 'b' ) {
+		        b++;
+		    } 
+		}
+		if(b>1) {
+			counter--;
+		}
+		int c = 0;
+		for( int some=0; some<word.length(); some++ ) {
+		    if( word.charAt(some) == 'c' ) {
+		        c++;
+		    } 
+		}
+		if(c>1) {
+			counter--;
+		}
+		int d = 0;
+		for( int some=0; some<word.length(); some++ ) {
+		    if( word.charAt(some) == 'd' ) {
+		        d++;
+		    } 
+		}
+		if(d>1) {
+			counter--;
+		}
+		int e = 0;
+		for( int some=0; some<word.length(); some++ ) {
+		    if( word.charAt(some) == 'e' ) {
+		        e++;
+		    } 
+		}
+		if(e>1) {
+			counter--;
+		}	
+		int f = 0;
+		for( int some=0; some<word.length(); some++ ) {
+		    if( word.charAt(some) == 'f' ) {
+		        f++;
+		    } 
+		}
+		if(f>1) {
+			counter--;
+		}
+		int g = 0;
+		for( int some=0; some<word.length(); some++ ) {
+		    if( word.charAt(some) == 'g' ) {
+		        c++;
+		    } 
+		}
+		if(g>1) {
+			counter--;
+		}
+		int h = 0;
+		for( int some=0; some<word.length(); some++ ) {
+		    if( word.charAt(some) == 'h' ) {
+		        h++;
+		    } 
+		}
+		if(h>1) {
+			counter--;
+		}
+		int i = 0;
+		for( int some=0; some<word.length(); some++ ) {
+		    if( word.charAt(some) == 'i' ) {
+		        i++;
+		    } 
+		}
+		if(i>1) {
+			counter--;
+		}
+		int j = 0;
+		for( int some=0; some<word.length(); some++ ) {
+		    if( word.charAt(some) == 'j' ) {
+		        j++;
+		    } 
+		}
+		if(j>1) {
+			counter--;
+		}
+		int k = 0;
+		for( int some=0; some<word.length(); some++ ) {
+		    if( word.charAt(some) == 'k' ) {
+		        k++;
+		    } 
+		}
+		if(k>1) {
+			counter--;
+		}
+		int l = 0;
+		for( int some=0; some<word.length(); some++ ) {
+		    if( word.charAt(some) == 'l' ) {
+		        l++;
+		    } 
+		}
+		if(l>1) {
+			counter--;
+		}
+		int m = 0;
+		for( int some=0; some<word.length(); some++ ) {
+		    if( word.charAt(some) == 'm' ) {
+		        m++;
+		    } 
+		}
+		if(m>1) {
+			counter--;
+		}
+		int n = 0;
+		for( int some=0; some<word.length(); some++ ) {
+		    if( word.charAt(some) == 'n' ) {
+		        n++;
+		    } 
+		}
+		if(n>1) {
+			counter--;
+		}
+		int o = 0;
+		for( int some=0; some<word.length(); some++ ) {
+		    if( word.charAt(some) == 'o' ) {
+		        o++;
+		    } 
+		}
+		if(o>1) {
+			counter--;
+		}
+		int p = 0;
+		for( int some=0; some<word.length(); some++ ) {
+		    if( word.charAt(some) == 'p' ) {
+		        p++;
+		    } 
+		}
+		if(p>1) {
+			counter--;
+		}
+		int q = 0;
+		for( int some=0; some<word.length(); some++ ) {
+		    if( word.charAt(some) == 'q' ) {
+		        q++;
+		    } 
+		}
+		if(q>1) {
+			counter--;
+		}
+		int r = 0;
+		for( int some=0; some<word.length(); some++ ) {
+		    if( word.charAt(some) == 'r' ) {
+		        r++;
+		    } 
+		}
+		if(r>1) {
+			counter--;
+		}
+		int s = 0;
+		for( int some=0; some<word.length(); some++ ) {
+		    if( word.charAt(some) == 's' ) {
+		        s++;
+		    } 
+		}
+		if(s>1) {
+			counter--;
+		}
+		int t = 0;
+		for( int some=0; some<word.length(); some++ ) {
+		    if( word.charAt(some) == 't' ) {
+		        t++;
+		    } 
+		}
+		if(t>1) {
+			counter--;
+		}
+		int u = 0;
+		for( int some=0; some<word.length(); some++ ) {
+		    if( word.charAt(some) == 'u' ) {
+		        u++;
+		    } 
+		}
+		if(u>1) {
+			counter--;
+		}
+		int v = 0;
+		for( int some=0; some<word.length(); some++ ) {
+		    if( word.charAt(some) == 'v' ) {
+		        v++;
+		    } 
+		}
+		if(v>1) {
+			counter--;
+		}
+		int x = 0;
+		for( int some=0; some<word.length(); some++ ) {
+		    if( word.charAt(some) == 'x' ) {
+		        x++;
+		    } 
+		}
+		if(x>1) {
+			counter--;
+		}
+		int y = 0;
+		for( int some=0; some<word.length(); some++ ) {
+		    if( word.charAt(some) == 'y' ) {
+		        y++;
+		    } 
+		}
+		if(y>1) {
+			counter--;
+		}
+		int z = 0;
+		for( int some=0; some<word.length(); some++ ) {
+		    if( word.charAt(some) == 'z' ) {
+		        z++;
+		    } 
+		}
+		if(z>1) {
+			counter--;
+		}
+	}
+	System.out.println(counter);
+  }
+}
